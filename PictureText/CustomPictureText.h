3c35200dd65961c9366e4415689caee02bea5034
runlin <runlin@gavin-wifi.local>
2016-12-19 17:49:56 +0800

    init ~

---
+++ PictureText/CustomPictureText.h
@@ -0,0 +1,13 @@
+//
+//  CustomPictureText.h
+//  PictureText
+//
+//  Created by runlin on 16/12/19.
+//  Copyright © 2016年 unis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomPictureText : UIView
+
+@end
---
+++ PictureText/CustomPictureText.m
@@ -0,0 +1,53 @@
+//
+//  CustomPictureText.m
+//  PictureText
+//
+//  Created by runlin on 16/12/19.
+//  Copyright © 2016年 unis. All rights reserved.
+//
+
+#import "CustomPictureText.h"
+
+@implementation CustomPictureText
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+
+
+- (UIView *)initPicture:(UIImage *)img withNSString:(NSString *)str withWidth:(CGFloat)width{
+    
+    //计算frame的高
+    //height = img+str
+    
+//    CGRect height =  img.size.width
+    
+    
+    
+    
+    
+    
+    
+//    self = [super initWithFrame:frame];
+//    if (self) {
+//    }
+//    return self;
+    
+    
+    
+    
+    return self;
+}
+
+
+
+
+
+
+@end
